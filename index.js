b7b21b41c82ed609a2c9c122e931641bec776c80
Akansha <akanshakaushik607@gmail.com>
2023-09-14 20:23:15 +0530

    ignore node modules

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get("/api/v1/playerDetails/",async(req,res)=>{
     });
 
 });
-
-app.listen(3000,() =>{
+const PORT = process.env.PORT || 3000
+app.listen(PORT,() =>{
     console.log("listening on port 3000");
 });
